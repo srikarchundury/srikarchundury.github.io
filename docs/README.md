34c2ad5f6292bccb2d356eca8494a77112111617
schundu3 <schundu3@ncsu.edu>
2025-09-06 22:01:34 -0400

    Deploy: build and publish site

--- docs/README.md
+++
@@ -1,4 +0,0 @@
-* Make all changes in main itself, and run this command also in main, but it deploys in gh-pages branch.
-``` 
-npm run build && npm run deploy 
-```
--- docs/do_things.sh
+++
@@ -1,5 +0,0 @@
-git add -A
-git commit -m "more changes"
-git push origin main
-NODE_OPTIONS=--openssl-legacy-provider npm run build
-NODE_OPTIONS=--openssl-legacy-provider npm run deploy
