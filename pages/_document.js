2c41c66764998eb6dd26c1c0d4a5b258e1aef160
schundu3 <schundu3@ncsu.edu>
2026-07-10 13:37:25 +0100

    minor

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head>
+				<meta name="google-site-verification" content="i1gvZZR8RB9nhnPIUbQ2Kj0MCVAk32jIlIg5Lah_AAk" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
